b3de5f5ea257e8505e2dadedbb7947cac3ed0a12
nov3o <ilya.bad111@yandex.ru>
2018-01-10 02:46:53 +0300

    New model finished. Made showing only last 5 posts in thread. A few fixes

--- board/models.py
+++ board/models.py
@@ -1,34 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
-"""
-class Post(models.Model):
-	author = models.CharField('Имя', max_length=70, blank=True)
-	title = models.CharField(max_length=100, null=True)
-	text = models.TextField()
-	published_date = models.DateTimeField(
-		blank=True, null=True)
-	thrd = models.IntegerField(
-		blank=True, null=True)
-
-	def publish(self):
-		self.thrd = self.id
-		self.save(update_fields=['thrd'])
-		self.published_date = timezone.now()
-		self.save()
-
-	def __str__(self):
-		return self.title
-"""
-
 class Thread(models.Model):
-	author = models.CharField('Имя', max_length=70, blank=True) #maybe i should remove null
-	title = models.CharField(max_length=100, blank=True, null=True) #maybe i should remove blank
+	author = models.CharField('Имя', max_length=70, blank=True)
+	title = models.CharField(max_length=100, blank=True)
 	text = models.TextField()
-	published_date = models.DateTimeField(
-		blank=True, null=True) #maybe I should remove blank and null
-	last_publish = models.DateTimeField(
-		blank=True, null=True) #maybe I should add edit=True
+	published_date = models.DateTimeField()
+	last_publish = models.DateTimeField() 
+	count_number = models.IntegerField()
 
 	def __str__(self):
 		return self.title
@@ -36,10 +15,9 @@ class Thread(models.Model):
 class Post(models.Model):
 	author = models.CharField('Имя', max_length=70, blank=True)
 	text = models.TextField()
-	published_date = models.DateTimeField(
-		blank=True, null=True) #maybe I should remove blank and null
-	thread_id = models.IntegerField(
-		blank=True, null=True) #maybe I should remove blank and null
+	published_date = models.DateTimeField()
+	thread_number = models.IntegerField()
+	count_number = models.IntegerField()
 
 	def __str__(self):
 		return self.text
--- board/views.py
+++ board/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Thread, Post
 from .forms import ThreadForm, PostForm
@@ -12,50 +11,59 @@ def post_list(request):
 			if thread.author == '':
 				thread.author = 'Товарищ'
 			thread.published_date = timezone.now()
+			thread.last_publish = thread.published_date
+			thread.count_number = get_max(Post.objects.all(), Thread.objects.all())
 			thread.save()
-			return redirect('thread_page', thread_id=thread.id)
+			return redirect('thread_page', thread_number=thread.count_number)
 	else:
 		form = ThreadForm()
 
-	#threads = Thread.objects.all()
-	#posts = Post.objects.all()
-	pairs = [ [thread, Post.objects.filter(thread_id=thread.id)] for thread in Thread.objects.all() ] 
-	# pairs is list of [ op-post, [post-in-thread-except-of-op-post-list] ]
-	#later change all() to ord_by('last_pub') 
-	"""
-	thrd_list = list(set([i.thrd for i in posts]))
-	last_pk_list = []
-	for i in thrd_list:
-		post_in_thread = Post.objects.filter(thrd=i)
-		last_pk_list.append(post_in_thread[len(post_in_thread)-1].pk)
-	sorted_thrd = []
-	for i in range(len(thrd_list)):
-		sorted_thrd.append(thrd_list[last_pk_list.index(max(last_pk_list))])
-		last_pk_list[last_pk_list.index(max(last_pk_list))] = 0
-	posts = []
-	for i in sorted_thrd:
-		posts.append(Post.objects.filter(thrd=i))
-	"""
-	return render(request, 'board/post_list.html', {'form': form, 'pairs': pairs}) 
-	#I'll extend QuerySet later
+	thread_nodes = []
+	for thread in Thread.objects.all().order_by('-last_publish'):
+		posts = Post.objects.filter(thread_number=thread.count_number)
+		if len(posts) > 5:
+			showed_posts = posts[len(posts)-5:]
+			missed = len(posts) - len(showed_posts)
+		else:
+			showed_posts = posts
+			missed = 0
+		thread_nodes.append([thread, showed_posts, missed])
 
-def thread_page(request, thread_id):
-	posts = Post.objects.filter(thread_id=thread_id)
+	return render(request, 'board/post_list.html', 
+		{'form': form, 'thread_nodes': thread_nodes}
+	)
+
+
+def thread_page(request, thread_number):
+	posts = Post.objects.filter(thread_number=thread_number)
+	op = Thread.objects.get(count_number=thread_number)
 
 	if request.method == "POST":
 		form = PostForm(request.POST)
 		if form.is_valid():
-			#Добавить изменение last_pub
 			new_post = form.save(commit=False)
 			if new_post.author == '':
 				new_post.author = 'Товарищ'
-			new_post.published_date = timezone.now()
-			new_post.thread_id = thread_id
+			new_post.published_date = timezone.now() 
+			new_post.thread_number = thread_number
+			new_post.count_number = get_max(Post.objects.all(), Thread.objects.all())
 			new_post.save()
-			return redirect('thread_page', thread_id=thread_id)
+
+			op.last_publish = new_post.published_date
+			op.save(update_fields=["last_publish"])
+
+			return redirect('thread_page', thread_number=thread_number)
 	else:
 		form = PostForm()
 
 	return render(request, 'board/thread_page.html', 
-		{'posts': posts, 'form': form, 'thread_id': thread_id}
+		{'posts': posts, 'form': form, 'thread_number': thread_number, 'op': op}
 	)
+
+
+def get_max(posts, threads):
+	lst = [i[len(i)-1].count_number for i in [posts, threads] if len(i) != 0]
+	if lst == []:
+		return 1
+	else:
+		return max(lst)+1
